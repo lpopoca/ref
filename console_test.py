1e9e4ce714ea7a599d2db510eabe87f2165815dd
Leonardo Popoca <tabsz.github@outlook.com>
2020-06-05 13:20:56 -0700

    Made a way to load json files

--- console_test.py
+++ console_test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import json
 
 def read_entry(ref_dict, ref_num):
     print(str(ref_num))
@@ -16,12 +17,42 @@ def new_entry(reference_dictionary):
     note = input("Enter note: ")
     create_ref(reference_dictionary,reference_number, reference_number_str, date, note)
 
+def load_file(file_name,):
+    ref_dict = json.load(open(file_name))
+    menu(ref_dict)
+
+def save_file(file_name, ref_dict):
+    j = json.dumps(ref_dict)
+    with open(file_name, 'w') as f:
+        f.write(j)
+        f.close()
+    return ref_dict
+
 def menu(ref_dict):
-    choice = input("Enter 1 to create new entry\n2 to read an entry\n0 to quit: ")
+    print("Enter a choice: ", end = "")
+    choice = input("""
+    1: Load a dictionary file
+    2: Save dictionary file
+    3: Create a new entry
+    4: Read a reference entry
+    0: to quit.
+    Enter ----> """)
     if choice == '1':
-        new_entry(ref_dict)
+        # Load a dictionary file
+        file_name = input("Enter file name: ")
+        file_name = file_name + ".json"
+        load_file(file_name)
         menu(ref_dict)
     elif choice == '2':
+        # Save a dictionary file
+        file_name = input("Enter a file name: ")
+        file_name = file_name + ".json"
+        save_file(file_name, ref_dict)
+        menu(ref_dict)
+    elif choice == '3':
+        new_entry(ref_dict)
+        menu(ref_dict)
+    elif choice == '4':
         reference_number = input("Enter a reference number: ")
         read_entry(ref_dict, reference_number)
         menu(ref_dict)
