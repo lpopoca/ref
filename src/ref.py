a4b0c6b5bd69281616e5a1b46af6f918f702feea
Leonardo Popoca <tabsz.github@outlook.com>
2020-09-17 21:14:11 -0700

    ref_controller class fix

--- src/ref.py
+++ src/ref.py
@@ -3,7 +3,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
-from refcontroller import *
+from ref_controller import *
 import datetime as dt
 
 app_controller = RefController()
