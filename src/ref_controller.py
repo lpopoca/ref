b4c55babc58c7566cb4eb95df2ef11814c001d79
Leonardo Popoca <tabsz.github@outlook.com>
2020-09-17 19:46:20 -0700

    Re added re_controller
    Had to readd the file. The previous naming scheme was causing a wierd issue.

---
+++ src/ref_controller.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import json
+
+class RefController:
+    """Controls the backend of Ref"""
+    def __init__(self, ref_dict={}):
+        self.ref_dict = ref_dict
+
+    def read_entry(self, ref_num):
+        """Reads an entry in the dictionary"""
+        return self.ref_dict[ref_num]["date"], self.ref_dict[ref_num]["note"]
+        
+    def new_entry(self, ref_num, ref_num_str, date, note):
+        """Creates a new dictionary entry"""
+        reference_number = ref_num
+        reference_number_str = str(reference_number)
+        reference_number = {'date' : date, 'note' : note}
+        self.ref_dict[reference_number_str] = reference_number
+
+    def load_file(self, file_name):
+        """Loads a json file as a dictionary"""
+        self.ref_dict = json.load(open(file_name))
+
+    def save_file(self, file_name):
+        """Saves the dictionary to the json file that is currently open"""
+        j = json.dumps(self.ref_dict, indent=4, sort_keys=True, default=str)
+        with open(file_name, 'w') as f:
+            f.write(j)
+            f.close()
